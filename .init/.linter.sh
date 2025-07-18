8816acd197a77a6b02d3eca3fdf7784f38f73841
Kavia AI <production@kavia.ai>
2025-07-18 15:30:34 +0000

    Auto-commit before merging to kavia-main - simple-notes-manager-7462aa4a

--- .init/.linter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /home/kavia/workspace/code-generation/simple-notes-manager-7462aa4a/notes_app_frontend
-./gradlew lint
-LINT_EXIT_CODE=$?
-if [ $LINT_EXIT_CODE -ne 0 ]; then
-   exit 1
-fi
-
